f1b0957ae0a0331f0995735652e77227fd7f27f7
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-10-27 19:43:20 +0300

    Add files via upload

---
+++ 03. JS Syntax Fundamentals - More Exercises/01-validity-checker.js
@@ -0,0 +1,16 @@
+function solve(x1, y1, x2, y2) {
+    
+    function checkDistance(x1, y1, x2, y2) {
+        let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        let status = "invalid";
+        if (Number.isInteger(distance)) {
+            status = "valid";
+        }
+    
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${status}`);
+    }    
+
+    checkDistance(x1, y1, 0, 0);
+    checkDistance(x2, y2, 0, 0);
+    checkDistance(x1, y1, x2, y2);
+}
