21e8b61a8182f23ddcd0fae5892e01b4b60708c8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-11-04 10:26:08 +0200

    Add files via upload

---
+++ 11. Objects and Classes - Exercises/09-make-a-dictionary.js
@@ -0,0 +1,11 @@
+function solve(inputArray) {
+    let dictionary = {};
+    for (const stringItem of inputArray) {
+        let jsonObject = JSON.parse(stringItem);
+        let [key, value] = Object.entries(jsonObject)[0];
+        dictionary[key] = value;
+    }
+    for (const [term, definition] of Object.entries(dictionary).sort()) {
+        console.log(`Term: ${term} => Definition: ${definition}`);                  
+    }
+}
