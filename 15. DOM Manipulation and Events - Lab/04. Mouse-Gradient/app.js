c793ed85ac3cb7873739e7f37de4c7dcadbc88aa
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-11-13 14:51:57 +0200

    Add files via upload

---
+++ 15. DOM Manipulation and Events - Lab/04. Mouse-Gradient/app.js
@@ -0,0 +1,14 @@
+function attachGradientEvents() {
+    const bar = document.getElementById("gradient");
+    const result = document.getElementById("result");
+
+    bar.addEventListener("mousemove", (e) => {
+        let power = e.offsetX / (e.target.clientWidth - 1) * 100;
+        power = Math.trunc(power);
+        result.textContent = power.toFixed() + "%";
+    })
+
+    bar.addEventListener("mouseout", (e) => {
+        result.textContent = ""
+    })
+}
