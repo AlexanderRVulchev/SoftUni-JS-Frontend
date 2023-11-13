35680fc8eac117597a87aabaccf7e8e334b7a0b4
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-11-13 14:52:47 +0200

    Add files via upload

---
+++ 15. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -0,0 +1,15 @@
+function validate() {
+    const email = document.getElementById("email");
+    const pattern = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "g");
+
+    email.addEventListener("change", (e) => {
+        const inputElement = e.target;
+        const inputText = inputElement.value;
+        const match = pattern.test(inputText);
+        if (match) {
+            inputElement.classList.remove("error");
+        } else {
+            inputElement.classList.add("error");
+        }
+    })
+}
