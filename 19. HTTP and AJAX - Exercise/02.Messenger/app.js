7d0aed0ad3e26e165edfa2ef4d3af7bf592e9ae3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-11-22 20:54:17 +0200

    Add files via upload

---
+++ 19. HTTP and AJAX - Exercise/02.Messenger/app.js
@@ -0,0 +1,30 @@
+function attachEvents() {
+    const baseUrl = "http://localhost:3030/jsonstore/messenger";
+    const [nameInput, contentInput, sendButton, refreshButton] = document.getElementsByTagName("input");
+    const messagesTextarea = document.getElementById("messages");
+    
+    sendButton.addEventListener("click", async () => {
+        const newMessage = {
+            author: nameInput.value,
+            content: contentInput.value
+        };
+        await fetch(baseUrl, {
+            method: "POST",
+            body: JSON.stringify(newMessage)
+        });
+        nameInput.value = "";
+        contentInput = "";
+    })
+
+    refreshButton.addEventListener("click", async () => {
+        const response = await fetch(baseUrl);
+        const messages = await response.json();
+        let messagesToDisplay = [];
+        for (const message of Object.values(messages)) {
+            messagesToDisplay.push(`${message.author}: ${message.content}`);
+        }
+        messagesTextarea.textContent = messagesToDisplay.join("\n");
+    })
+}
+
+attachEvents();
