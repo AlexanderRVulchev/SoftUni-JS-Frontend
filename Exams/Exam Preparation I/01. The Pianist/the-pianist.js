bb3329c5d87088e3f4610a5486e4d21c910d081b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-12-10 21:26:04 +0200

    Add files via upload

--- Exams/Exam Preparation I/01. The Pianist/the-pianist.js
+++ Exams/Exam Preparation I/01. The Pianist/the-pianist.js
@@ -59,19 +59,3 @@ function solve(input) {
         }
     }
 }
-
-solve(
-    [
-        '4',
-        'Eine kleine Nachtmusik|Mozart|G Major',
-        'La Campanella|Liszt|G# Minor',
-        'The Marriage of Figaro|Mozart|G Major',
-        'Hungarian Dance No.5|Brahms|G Minor',
-        'Add|Spring|Vivaldi|E Major',
-        'Remove|The Marriage of Figaro',
-        'Remove|Turkish March',
-        'ChangeKey|Spring|C Major',
-        'Add|Nocturne|Chopin|C# Minor',
-        'Stop'
-      ]      
-);
