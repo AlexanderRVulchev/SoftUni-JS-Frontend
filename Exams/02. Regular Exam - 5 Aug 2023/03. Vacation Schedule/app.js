4dfd2ccf1b72b67cb3cdd3c13a455ed373bc10ec
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-12-10 11:12:39 +0200

    Add files via upload

--- Exams/02. Regular Exam - 5 Aug 2023/03. Vacation Schedule/app.js
+++ Exams/02. Regular Exam - 5 Aug 2023/03. Vacation Schedule/app.js
@@ -1,6 +1,6 @@
 function onLoad() {
     const baseUrl = "http://localhost:3030/jsonstore/tasks/";
-    let idToEdit = "";
+    let idForEdit = "";
 
     const nameInput = document.getElementById("name");
     const daysInput = document.getElementById("num-days");
@@ -8,136 +8,125 @@ function onLoad() {
     const addButton = document.getElementById("add-vacation");
     const editButton = document.getElementById("edit-vacation");
     const loadButton = document.getElementById("load-vacations");
-    const listDiv = document.getElementById("list");
+    const listUl = document.getElementById("list");
 
-    listDiv.innerHTML = "";
-
-    loadButton.addEventListener("click", loadVacations);
-    addButton.addEventListener("click", addVacation);
-    editButton.addEventListener("click", editVacation);
+    loadButton.addEventListener("click", load);
+    addButton.addEventListener("click", add);
+    editButton.addEventListener("click", edit);
 
     // -- Event handlers
 
-    async function loadVacations() {        
-        const vacations = await fetchAllVacations();
-
+    async function load() {
         editButton.disabled = true;
-        listDiv.innerHTML = "";
-
-        for (const vacation of Object.values(vacations)) {
-            const containerDiv = buildContainerDiv(vacation);
-            listDiv.appendChild(containerDiv);
+        listUl.innerHTML = "";
+        const vacations = await fetchGet();
+        for (const vacation of vacations) {
+            const [name, days, date, id] = deconstructVacation(vacation);
+            const container = buildVacationContainer(name, days, date, id);
+            listUl.appendChild(container);
         }
     }
 
-    async function addVacation() {
-        [name, date, days] = getInput();
-
-        const data = { name, date, days, };
-
+    async function add() {
+        const [name, days, date] = readInput();
         clearInput();
+        await fetchPost(name, days, date);
+        await load();
+    }
 
+    async function edit() {
+        const [name, days, date] = readInput();
+        clearInput();
+        await fetchPut(name, days, date, idForEdit);
+        await load();
+    }
+
+    // -- Http requests
+
+    async function fetchGet() {
+        const response = await fetch(baseUrl);
+        const vacationsObj = await response.json();
+        const vacations = Object.values(vacationsObj);
+        return Array.from(vacations);
+    }
+
+    async function fetchPost(name, days, date) {
+        const data = { name, days, date };
         await fetch(baseUrl, {
             method: "POST",
             body: JSON.stringify(data)
-        });
-
-        loadVacations();
+        })
     }
 
-    async function changeVacation(e) {
-        const vacationElement = e.currentTarget.parentElement;
-        const nameH2 = vacationElement.getElementsByTagName("h2")[0];
-
-        const vacations = await fetchAllVacations();
-        const vacation = Object.values(vacations).find(v => v.name === nameH2.textContent);
-
-        idToEdit = vacation._id;
-        const name = vacation.name;
-        const date = vacation.date;
-        const days = vacation.days;
-        setInput(name, date, days);
-
-        listDiv.removeChild(vacationElement);
-
-        editButton.disabled = false;
-        addButton.disabled = true;
-    }
-
-    function editVacation() {
-        [name, date, days] = getInput();
-        const data = { name, date, days, _id: idToEdit };
-
-        fetch(baseUrl + idToEdit, {
+    async function fetchPut(name, days, date, id) {
+        const data = { name, days, date };
+        await fetch(baseUrl + id, {
             method: "PUT",
             body: JSON.stringify(data)
         })
-        .then(clearInput())
-        .then(loadVacations())
-        .then(() => {
-            editButton.disabled = true;
-            addButton.disabled = false;
-        })     
     }
 
-    async function doneVacation(e) {
-        const vacationElement = e.currentTarget.parentElement;
-        const nameH2 = vacationElement.getElementsByTagName("h2")[0];
-        
-        const vacations = await fetchAllVacations();
-        const vacation = Object.values(vacations).find(v => v.name === nameH2.textContent);
-        const idToDelete = vacation._id;
-
-        await fetch(baseUrl + idToDelete, {
+    async function fetchDelete(id) {
+        await fetch(baseUrl + id, {
             method: "DELETE"
-        });
-
-        loadVacations();
+        })
     }
 
     // -- Helper functions
 
-    async function fetchAllVacations() {
-        const response = await fetch(baseUrl);
-        const vacations = await response.json();
-        return vacations;
-    }
-
-    function getInput() {
+    function readInput() {
         const name = nameInput.value;
-        const date = dateInput.value;
         const days = daysInput.value;
-        return [name, date, days];
+        const date = dateInput.value;
+        return [name, days, date];
     }
 
-    function setInput(name, date, days) {
+    function setInput(name, days, date) {
         nameInput.value = name;
-        dateInput.value = date;
         daysInput.value = days;
+        dateInput.value = date;
     }
 
     function clearInput() {
-        nameInput.value = "";
-        dateInput.value = "";
-        daysInput.value = "";
+        setInput("", "", "");
+    }
+
+    function deconstructVacation(vacation) {
+        const name = vacation.name;
+        const days = vacation.days;
+        const date = vacation.date;
+        const id = vacation._id;
+        return [name, days, date, id];
     }
 
     // -- Html builders
 
-    function buildContainerDiv(vacation) {
-        const nameH2 = buildHtmlElement("h2", vacation.name, null, null);
-        const dateH3 = buildHtmlElement("h3", vacation.date, null, null);
-        const daysH3 = buildHtmlElement("h3", vacation.days, null, null);
-        const changeButton = buildHtmlElement("button", "Change", null, "change-btn");
-        const doneButton = buildHtmlElement("button", "Done", null, "done-btn");
+    function buildVacationContainer(name, days, date, id) {
+        const nameH2 = buildHtmlElement("h2", name, null, null);
+        const dateH3 = buildHtmlElement("h3", date, null, null);
+        const daysH3 = buildHtmlElement("h3", days, null, null);
+        const changeButton = buildHtmlElement("button", "Change", null, ["change-btn"]);
+        const doneButton = buildHtmlElement("button", "Done", null, ["done-btn"]);
 
-        changeButton.addEventListener("click", changeVacation);
-        doneButton.addEventListener("click", doneVacation);
+        const containerDiv = buildHtmlElement("div", null, null, ["container"], nameH2, dateH3, daysH3, changeButton, doneButton);
 
-        return buildHtmlElement("div", null, null, "container", nameH2, dateH3, daysH3, changeButton, doneButton);
+        changeButton.addEventListener("click", async () => {
+            setInput(name, days, date);
+            idForEdit = id;
+            addButton.disabled = true;
+            editButton.disabled = false;
+            containerDiv.remove();
+        });
+
+        doneButton.addEventListener("click", async () => {
+            await fetchDelete(id);
+            await load();
+        })
+
+        return containerDiv;
     }
 
-    function buildHtmlElement(tag, text, id, className, ...children) {
+    function buildHtmlElement(tag, text, id, classNames, ...children) {
         const element = document.createElement(tag);
         if (text !== null) {
             element.textContent = text;
@@ -145,8 +134,10 @@ function onLoad() {
         if (id !== null) {
             element.id = id;
         }
-        if (className !== null) {
-            element.classList.add(className);
+        if (classNames !== null) {
+            classNames.forEach(className => {
+                element.classList.add(className);
+            });
         }
 
         for (const child of children) {
