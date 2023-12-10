90c61770d5a14e2b33d1859291ec7761af259473
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-12-10 13:02:29 +0200

    Add files via upload

--- Exams/03. Retake Exam - 20 Apr 2023/02. Post Task/app.js
+++ Exams/03. Retake Exam - 20 Apr 2023/02. Post Task/app.js
@@ -1,89 +1,66 @@
-window.addEventListener("load", solve);
+//window.addEventListener("load", solve);
 
 function solve() {
     const titleInput = document.getElementById("task-title");
     const categoryInput = document.getElementById("task-category");
-    const contentTextArea = document.getElementById("task-content");
+    const contentInput = document.getElementById("task-content");
     const publishButton = document.getElementById("publish-btn");
     const reviewUl = document.getElementById("review-list");
     const publishedUl = document.getElementById("published-list");
 
     publishButton.addEventListener("click", publish);
 
-
-    // -- Event handlers
-
     function publish() {
         const [title, category, content] = readInput();
-
         if (title && category && content) {
             clearInput();
-            reviewUl.appendChild(buildReviewLi(title, category, content));
+            const reviewPostLi = buildReviewLi(title, category, content);
+            reviewUl.appendChild(reviewPostLi);
         }
     }
-
-    function edit(e) {
-        const reviewLiElement = e.currentTarget.parentElement;
-        const [title, category, content] = extractTextsFromArticle(reviewLiElement);
-        setInput(title, category, content);
-        reviewLiElement.remove();
-    }
-
-    function post(e) {
-        const reviewLiElement = e.currentTarget.parentElement;
-        const [title, category, content] = extractTextsFromArticle(reviewLiElement);
-        reviewLiElement.remove();
-
-        const articleElement = buildArticleItem(title, category, content);
-        const publishedLiElement = buildHtmlElement("li", null, null, ["rpost"], articleElement);
-        publishedUl.appendChild(publishedLiElement);
-    }
-
-    // -- Helper functions
 
     function readInput() {
         const title = titleInput.value;
         const category = categoryInput.value;
-        const content = contentTextArea.value;
+        const content = contentInput.value;
         return [title, category, content];
-    }
-
-    function clearInput() {
-        titleInput.value = "";
-        categoryInput.value = "";
-        contentTextArea.value = "";
     }
 
     function setInput(title, category, content) {
         titleInput.value = title;
         categoryInput.value = category;
-        contentTextArea.value = content;
-    }
-
-    function extractTextsFromArticle(liElement) {
-        const innerArticleElements = Array.from(liElement.querySelector("article").children);
-
-        const title = innerArticleElements[0].textContent;
-        const category = innerArticleElements[1].textContent.slice("Category: ".length);
-        const content = innerArticleElements[2].textContent.slice("Content: ".length);
+        contentInput.value = content;
         return [title, category, content];
     }
 
-    // -- Html builders
+    function clearInput() {
+        setInput("", "", "");
+    }
+
 
     function buildReviewLi(title, category, content) {
-        const article = buildArticleItem(title, category, content);
+        const article = buildArticle(title, category, content);
         const editButton = buildHtmlElement("button", "Edit", null, ["action-btn", "edit"]);
-        const postButton = buildHtmlElement("button", "Post", null, ["action-btn", "post"]);
+        const postbutton = buildHtmlElement("button", "Post", null, ["action-btn", "post"]);
 
-        editButton.addEventListener("click", edit);
-        postButton.addEventListener("click", post);
+        const li = buildHtmlElement("li", null, null, ["rpost"], article, editButton, postbutton);
 
-        const li = buildHtmlElement("li", null, null, ["rpost"], article, editButton, postButton);
+        editButton.addEventListener("click", () => {
+            li.remove();
+            setInput(title, category, content);
+        });
+
+        postbutton.addEventListener("click", () => {
+            li.remove();
+            const article = buildArticle(title, category, content);
+            const publishedLi = buildHtmlElement("li", null, null, ["rpost"], article);
+            publishedUl.appendChild(publishedLi);
+        });
+
         return li;
     }
 
-    function buildArticleItem(title, category, content) {
+    function buildArticle(title, category, content) {
         const titleH4 = buildHtmlElement("h4", title, null, null);
         const categoryP = buildHtmlElement("p", `Category: ${category}`, null, null);
         const contentP = buildHtmlElement("p", `Content: ${content}`, null, null);
