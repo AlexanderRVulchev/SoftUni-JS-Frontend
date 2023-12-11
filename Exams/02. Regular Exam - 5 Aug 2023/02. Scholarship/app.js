8023fcead2b11a089ca9efc388ccf39423d88998
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-12-11 20:42:25 +0200

    Add files via upload

--- Exams/02. Regular Exam - 5 Aug 2023/02. Scholarship/app.js
+++ Exams/02. Regular Exam - 5 Aug 2023/02. Scholarship/app.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", solve);
+//window.addEventListener("load", solve);
 
 function solve() {
   const studentInput = document.getElementById("student");
@@ -8,79 +8,66 @@ function solve() {
   const previewUl = document.getElementById("preview-list");
   const candidatesUl = document.getElementById("candidates-list");
 
-  nextButton.addEventListener("click", nextEvent);
+  nextButton.addEventListener("click", add);
 
-  // -- Event handlers
-
-  function nextEvent() {
-    const student = studentInput.value;
-    const university = universityInput.value;
-    const score = scoreInput.value;
-
-    studentInput.value = "";
-    universityInput.value = "";
-    scoreInput.value = "";
-    nextButton.disabled = true;
-
+  function add() {
+    const [student, university, score] = readInput();
     if (student && university && score) {
-      previewUl.appendChild(buildApplicationItem(student, university, score));
+      clearInput();
+      const applicationLi = buildApplicationLi(student, university, score);
+      previewUl.appendChild(applicationLi);
+      nextButton.disabled = true;
     }
   }
 
-  function editEvent() {
-    const [student, university, score] = readAndClearPreviewList();
-
-    studentInput.value = student;
-    universityInput.value = university;
-    scoreInput.value = score;
-
-    nextButton.disabled = false;
-  }
-
-  function applyEvent() {
-    const [student, university, score] = readAndClearPreviewList();
-
-    const article = buildArticleElement(student, university, score);
-    const li = buildHtmlElement("li", null, null, ["application"], article);
-    candidatesUl.appendChild(li);
-
-    nextButton.disabled = false;
-  }
-
-  // -- Helper functions
-
-  function readAndClearPreviewList() {
-    const article = document.querySelector("#preview-list li.application article");
-    const [studentH4, universityP, scoreP] = Array.from(article.children);
-
-    const student = studentH4.textContent;
-    const university = universityP.textContent.slice("University: ".length);
-    const score = scoreP.textContent.slice("Score: ".length);
-
-    previewUl.innerHTML = "";
+  function readInput() {
+    const student = studentInput.value;
+    const university = universityInput.value;
+    const score = scoreInput.value;
     return [student, university, score];
   }
 
-  // -- Html builders
+  function setInput(student, university, score) {
+    studentInput.value = student;
+    universityInput.value = university;
+    scoreInput.value = score;
+  }
 
-  function buildApplicationItem(student, university, score) {
-    const article = buildArticleElement(student, university, score);
+  function clearInput() {
+    setInput("", "", "");
+  }
 
+  function buildApplicationLi(student, university, score) {
+    const article = buildArticle(student, university, score);
     const editButton = buildHtmlElement("button", "edit", null, ["action-btn", "edit"]);
     const applyButton = buildHtmlElement("button", "apply", null, ["action-btn", "apply"]);
 
-    editButton.addEventListener("click", editEvent);
-    applyButton.addEventListener("click", applyEvent);
+    const applicationLi = buildHtmlElement("li", null, null, ["application"], article, editButton, applyButton);
 
-    return buildHtmlElement("li", null, null, ["application"], article, editButton, applyButton);
+    editButton.addEventListener("click", () => {
+      nextButton.disabled = false;
+      setInput(student, university, score);
+      applicationLi.remove();
+    });
+
+    applyButton.addEventListener("click", () => {
+      applicationLi.remove();
+      const article = buildArticle(student, university, score);
+      const li = buildHtmlElement("li", null, null, ["application"], article);
+      candidatesUl.appendChild(li);
+      nextButton.disabled = false;
+    })
+
+    return applicationLi;
   }
 
-  function buildArticleElement(student, university, score) {
+  function buildArticle(student, university, score) {
     const studentH4 = buildHtmlElement("h4", student, null, null);
     const universityP = buildHtmlElement("p", `University: ${university}`, null, null);
     const scoreP = buildHtmlElement("p", `Score: ${score}`, null, null);
 
-    return buildHtmlElement("article", null, null, null, studentH4, universityP, scoreP);
+    const article = buildHtmlElement("article", null, null, null, studentH4, universityP, scoreP);
+    return article;
   }
 
   function buildHtmlElement(tag, text, id, classNames, ...children) {
